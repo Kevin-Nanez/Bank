3c39e689a19bc6cb37143946b539baae9a52a816
Kevin <kevinn2304@gmail.com>
2022-11-15 13:15:50 -0600

    upDATE

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ class Server {
 
     constructor() {
         this.app    = express();
-        this.port   = process.env.PORT;
+        this.port   = 8080;
         this.server = require('http').createServer( this.app );
         this.io     = require('socket.io')( this.server );
 
